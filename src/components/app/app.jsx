27592a4c7e9c9db9c6665141629f1cfaa7433f1e
dbogdanov <dbogdanov87@yandex.ru>
2019-11-13 20:01:57 +0300

    Добавил отрисовку одинаковык карточек с разными названиями. Добавил prop types.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import MainScreen from "../main-screen/main-screen.jsx";
 
 const App = () => {
-  return <MainScreen/>
+  const cardNames=[`Beautiful &amp; luxurious apartment at great location`,
+    `Wood and stone place`,
+    `Canal View Prinsengracht`,
+    `Nice, cozy, warm big bed apartment`,
+  ];
+  return <MainScreen cardNames={cardNames}/>
 };
 
 export default App;
