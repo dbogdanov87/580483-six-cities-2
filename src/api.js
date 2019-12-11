8453ce2e7802f9f66df429b02cc4deba4852572f
dbogdanov <dbogdanov87@yandex.ru>
2019-12-11 21:02:30 +0300

    Доработал api и доавбил его в index.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 
+export const createAPI = (onLoginFail) => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-2.appspot.com/six-cities`,
+    timeout: 1000 * 5,
+    withCredentials: true,
+  });
 
-export const api = axios.create({
-  baseURL: `https://htmlacademy-react-2.appspot.com/six-cities`,
-  timeout: 1000 * 5,
-  withCredentials: true,
-});
+  const onSuccess = (response) => response;
+  const onFail = (err) => {
+    if (err.status === 401) {
+      onLoginFail();
+      return;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
