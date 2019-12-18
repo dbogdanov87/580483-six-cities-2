33bfad044e855ab70d78c84ca641733b17180e35
dbogdanov <dbogdanov87@yandex.ru>
2019-12-18 14:11:34 +0300

    Д12 +

--- src/components/favorites-items/favorites-items.jsx
+++ src/components/favorites-items/favorites-items.jsx
@@ -1,36 +1,30 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import FavoritesPlace from "../favorites-places/favorites-places.jsx";
 
-class FavoritesItems extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const FavoritesItems = (props) => {
+  const {
+    favorites,
+    city,
+  } = props;
 
-  render() {
-    const {
-      favorites,
-      city,
-    } = this.props;
-
-    return (
-      <li className="favorites__locations-items">
-        <div className="favorites__locations locations locations--current">
-          <div className="locations__item">
-            <a className="locations__item-link" href="#">
-              <span>{city}</span>
-            </a>
-          </div>
+  return (
+    <li className="favorites__locations-items">
+      <div className="favorites__locations locations locations--current">
+        <div className="locations__item">
+          <a className="locations__item-link" href="#">
+            <span>{city}</span>
+          </a>
         </div>
-        <div className="favorites__places">
-          {
-            favorites.map((favorite, index) => <FavoritesPlace key={index} favorite={favorite}/>)
-          }
-        </div>
-      </li>
-    );
-  }
-}
+      </div>
+      <div className="favorites__places">
+        {
+          favorites.map((favorite, index) => <FavoritesPlace key={index} favorite={favorite}/>)
+        }
+      </div>
+    </li>
+  );
+};
 
 FavoritesItems.propTypes = {
   city: PropTypes.string,
